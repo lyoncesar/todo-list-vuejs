6e9e627e30c27db94287659bf6f47f2643a2f96e
Lyon Cesar <lyoncesar@gmail.com>
2020-12-10 00:01:38 -0300

    Add a style tag and his styled css.
    Apply the styled css using the class attribute on html tags

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,17 +1,20 @@
 <template>
   <!-- The shorthand to 'v-on' is '@' -->
   <form v-on:submit.prevent="onSubmit">
-    <label for="new-todo-input">
-      What needs to be done?
-    </label>
+    <h2 class="label-wrapper">
+      <label for="new-todo-input">
+        What needs to be done?
+      </label>
+    </h2>
     <input
       type="text"
       id="new-todo-input"
       name="new-todo"
       autocomplete="off"
       v-model.lazy.trim="label"
+      class="input__lg"
     />
-    <button type="submit">
+    <button type="submit" class="btn btn__primary btn__lg">
       Add
     </button>
   </form>
